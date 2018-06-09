b83f8a72e7ffc430b35b7db8d702e85a65e36193
natinusala <natinusala@gmail.com>
2018-06-09 15:58:41 +0200

    Cert generation

--- src/libgamestream/mkcert.c
+++ src/libgamestream/mkcert.c
@@ -20,14 +20,18 @@
 
 #include <wolfssl/options.h>
 #include <wolfssl/wolfcrypt/rsa.h>
+#include <wolfssl/wolfcrypt/asn_public.h>
 
 #define RSA_KEY_SIZE 2048
+#define VALIDITY_YEARS 10
 
 void mkcert(const char* cert_file, const char* p12_file, const char* key_file)
 {
 	RsaKey key;
 	WC_RNG rng;
 	int ret;
+	
+	//Init RNG
 	
 	ret = wc_InitRng(&rng);
 	if (ret != 0)
@@ -36,6 +40,8 @@ void mkcert(const char* cert_file, const char* p12_file, const char* key_file)
 		return;
 	}
 	
+	//Init RSA
+	
 	ret = wc_InitRsaKey(&key, NULL);
 	if (ret != 0)
 	{
@@ -43,14 +49,66 @@ void mkcert(const char* cert_file, const char* p12_file, const char* key_file)
 		return;
 	}
 	
+	//Make RSA
+	
 	ret = wc_MakeRsaKey(&key, RSA_KEY_SIZE, 65537, &rng);
     if(ret != 0) {
         printf("[ERROR] Unable to generate RSA key : %d\n", ret);
     }
 	
-	// TODO Generate a certificate + storage file and save everything
+	//Save RSA
+	
+	//TODO
+	
+	//Make cert
+	
+	Cert cert;
+	
+	wc_InitCert(&cert);
+	
+	strncpy(cert.subject.commonName, "NVIDIA GameStream Client", CTC_NAME_SIZE);
+	strncpy(cert.issuer.commonName, "NVIDIA GameStream Client", CTC_NAME_SIZE);
+	
+	cert.daysValid = VALIDITY_YEARS * 365;
+	
+	cert.serial[0] = 0;
+	cert.serialSz = 1;
+	
+	cert.isCA = 0;
+    cert.sigType = CTC_SHAwRSA;
+	
+	byte der_cert[4096];
+	
+	int der_size = wc_MakeSelfCert(&cert, der_cert, sizeof(der_cert), &key, &rng);
+	
+	if (der_size < 0)
+	{
+		printf("[ERROR] Unable to generate certificate : %d\n", ret);
+		return;
+	}
+	
+	//Save cert
+	
+	byte pem_cert[4096];
+	
+	int pem_size = wc_DerToPem(der_cert, der_size, pem_cert, sizeof(pem_cert), CERT_TYPE);
+	
+	if (pem_size < 0)
+	{
+		printf("[ERROR] Unable to convert certificate : %d\n", ret);
+		return;
+	}
+	
+	FILE* fd = fopen(cert_file, "w");
+	
+	fwrite(pem_cert, pem_size, 1, fd);
+	
+	fclose(fd);
+	
+	// TODO storage file
+	
+	//Cleanup
 	
 	wc_FreeRsaKey(&key);
-	wc_FreeRng(&rng);
-	
+	wc_FreeRng(&rng);	
 }
